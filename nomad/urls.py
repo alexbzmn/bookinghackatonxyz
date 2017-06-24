48dcf8fa0bda88bd00a125e1eee48adfd2a0143d
alexbzmn <crashius@gmail.com>
2017-06-24 13:22:35 +0200

    project skeleton(with sample app from django)

---
+++ nomad/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'nomad'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(create-question/$)', views.create_question_view, name='createq'),
+    url(r'^(create-question/create$)', views.create_question_create, name='createqaction'),
+]
